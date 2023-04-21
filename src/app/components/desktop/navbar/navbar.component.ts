764b8fa9acccdcc59db50f8ecbd0840e1ac2b2db
Julien Constant <julienconstant190@gmail.com>
2023-04-22 00:01:41 +0200

    Added links to navbar items

--- src/app/components/desktop/navbar/navbar.component.ts
+++ src/app/components/desktop/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { PageService } from 'src/app/services/page.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -7,5 +8,8 @@ import { UserService } from 'src/app/services/user.service';
 	styleUrls: ['./navbar.scss'],
 })
 export class DesktopNavbarComponent {
-	public constructor(@Inject(UserService) public readonly u: UserService) {}
+	public constructor(
+		@Inject(UserService) public readonly u: UserService,
+		@Inject(PageService) public readonly p: PageService,
+	) {}
 }
