20c0cc72a3aa01e1d232603c9170d451e6efd016
Julien Constant <julienconstant190@gmail.com>
2023-05-23 02:35:28 +0200

    Fix default header

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,6 +2,6 @@
  * Default headers for all requests
  */
 export const DEFAULT_HEADERS: { [key: string]: string | string[] } = {
-	Authorization: `${sessionStorage.getItem('token')}`,
+	Authorization: `${sessionStorage.getItem('user_token')}`,
 	'Accept-Language': localStorage.getItem('lang') ?? 'en-US',
 };
