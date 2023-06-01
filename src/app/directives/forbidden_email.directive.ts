221fdfcb32e763b1b0429096b5a70693a35c02d6
Julien Constant <julienconstant190@gmail.com>
2023-06-01 18:56:24 +0200

    Added a whitelist on forbiddenEmailValidator

--- src/app/directives/forbidden_email.directive.ts
+++ src/app/directives/forbidden_email.directive.ts
@@ -1,5 +1,7 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
+const WHITELIST = ['ae@utbm.fr', 'ae.info@utbm.fr'];
+
 /**
  * Check if the given email is not a forbidden one.
  * @param {...string} emails Array of forbidden emails (e.g. '@utbm.fr')
@@ -7,7 +9,9 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
  */
 export function forbiddenEmailValidator(...emails: string[]): ValidatorFn {
 	return (control: AbstractControl): ValidationErrors | null => {
-		const forbidden = emails.find((e) => control.value && control.value.endsWith(e));
+		const forbidden = emails.find(
+			(e) => control.value && control.value.endsWith(e) && !WHITELIST.includes(control.value),
+		);
 
 		return forbidden ? { forbidden_email: true } : null;
 	};
