635b22967b17e4c9938228fd52367420f919819b
Julien Constant <julienconstant190@gmail.com>
2023-06-01 15:59:20 +0200

    Added JSDoc

--- src/app/directives/date.directive.ts
+++ src/app/directives/date.directive.ts
@@ -1,6 +1,11 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 
+/**
+ * Check if the field is at least the min age
+ * @param {number} min The minimum age to be accepted
+ * @returns {ValidatorFn} The angular validator function
+ */
 export function ageMinValidator(min = environment.REGISTER_AGE_MIN): ValidatorFn {
 	return (control: AbstractControl): ValidationErrors | null => {
 		if (!control.value) return null;
@@ -11,6 +16,11 @@ export function ageMinValidator(min = environment.REGISTER_AGE_MIN): ValidatorFn
 	};
 }
 
+/**
+ * Check if the field is at most the max age
+ * @param {number} max The maximum age to be accepted
+ * @returns {ValidatorFn} The angular validator function
+ */
 export function ageMaxValidator(max = environment.REGISTER_AGE_MAX): ValidatorFn {
 	return (control: AbstractControl): ValidationErrors | null => {
 		if (!control.value) return null;
@@ -21,6 +31,11 @@ export function ageMaxValidator(max = environment.REGISTER_AGE_MAX): ValidatorFn
 	};
 }
 
+/**
+ * Check that the field is not set in the future
+ * @param {AbstractControl} control The control to check
+ * @returns {ValidationErrors | null} The error if the field is in the future
+ */
 export function notInFutureValidator(control: AbstractControl): ValidationErrors | null {
 	if (!control.value) return null;
 
